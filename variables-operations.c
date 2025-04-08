9c57205a69058a639a1a1cc90698b2009404841b
Alyssa Alvaradejo <assylalva@gmail.com>
2025-04-08 14:41:21 +0000

    inputs, variables operations

--- variables-operations.c
+++ variables-operations.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 int main(){
 //area
-   int length = 6;
-   int width =4;
-  
+   int length;
+   int width;
+
+   printf("Enter the rectangle width:");
+   scanf("%d", &width);
+   printf("Enter the rectangle length:");
+   scanf("%d", &length);
+
    int area = (length * width);
    int perimiter = (length * 2) + (width * 2);
-
+ printf("Area:%d\n",area);
+ printf("Perimiter:%d\n", perimiter);
 
 // fahrenheit to celsius
-int fahrenheit = 45;
-int celsius = (fahrenheit-32)/1.8;
+ int fahrenheit;
+
+ printf("Enter the fahrenheit degrees: ");
+ scanf("%d", &fahrenheit);
+ int celsius = (fahrenheit-32)/1.8;
+ printf("%d°F = %d°C\n", fahrenheit, celsius);
 
 //minutes into hours
 int minutes = 349;
+
+printf("Enter the minutes: ");
+scanf("%d", &minutes);
 int hours = minutes/60;
-//int hoursleft = minutes - hours * 60;
 int modulus = minutes % 60;
-
-//area
-printf("Area:%d\n",area);
-printf("Perimiter:%d\n", perimiter);
-
-//grados
-printf("%d°F = %d°C\n", fahrenheit, celsius);
 
 //horas
 printf("%d minutes is equal to %d hours and %d minutes\n",minutes, hours, modulus);
