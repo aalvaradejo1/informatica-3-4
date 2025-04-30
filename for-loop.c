331b419639318ba0e37626e740dd612489385408
Alyssa Alvaradejo <assylalva@gmail.com>
2025-04-30 14:39:12 +0000

    for loop

---
+++ for-loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+//excercise 1
+int n;
+printf("Enter a positive number: ");
+scanf("%d", &n);
+
+int i;
+int sum =0;
+for(i = 1; i < n + 1; ++i){
+    sum = sum +i; 
+}
+
+printf("%d\n", sum);
+
+}
--- if-challenge.c
+++ if-challenge.c
@@ -6,11 +6,7 @@ int year;
 printf("Enter a year:");
 scanf("%d", &year);
 
-if (year % 4 == 0 && year % 100 != 0){
-    if (year % 100 != 0);
-    printf("%d It's a leap year\n", year);
-} else (year % 400 ==0){
-    printf("It's a leap year");
-}
+if ((year % 4 == 0 && year % 100 != 0)|| year % 400){
+}    
 return 0;
 }
