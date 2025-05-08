92536029999b408561c43f5eca04cdc4b9a231b9
Alyssa Alvaradejo <assylalva@gmail.com>
2025-05-08 16:20:51 +0000

    exponents

---
+++ exponents.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+
+    int base;
+    int exp;
+    int count = 1;
+
+    printf("Enter the base number: ");
+    scanf("%d", &base);
+
+    printf("Enter the exponent: ");
+    scanf("%d", &exp);
+
+    int ans = base;
+    
+    while (count < exp ){
+        ans = ans * base;
+        ++count;
+    }
+    printf("%d ^ %d = %d\n", base, exp, ans);
+
+}
